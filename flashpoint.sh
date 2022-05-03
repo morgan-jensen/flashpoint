0ad4ea592da4a07ed2efe90d79bac52e6cec70eb
Morgan Jensen <76416843+morgan-jensen@users.noreply.github.com>
2022-05-03 09:31:51 -0600

    Update flashpoint.sh

--- flashpoint.sh
+++ flashpoint.sh
@@ -2,6 +2,4 @@ yes | sudo dnf update
 
 echo -e "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h" | sudo tee -a /etc/yum.repos.d/vscodium.repo
 
-yes | sudo dnf install codium python3-pip
-
-pip install scikit-image
+yes | sudo dnf install codium python3-pip && pip install scikit-image
